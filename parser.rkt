8fbc051ad88a8edfaa1653df723e47f9eb9b0e5b
Bdown4587 <55153467+Bdown4587@users.noreply.github.com>
2022-10-13 09:45:45 -0400

    Update parser.rkt

--- parser.rkt
+++ parser.rkt
@@ -3,24 +3,6 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname parser) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 #lang racket
 (require "utility.rkt")
-(define neo-parser
-  (lambda (neo-code)
-    (cond
-      ((null? neo-code) '())
-      ((number? neo-code) (list 'num-exp neo-code))
-      ((symbol? neo-code) (list 'var-exp neo-code))
-       ((equal? (car neo-code) 'bool) (neo-bool-code-parser neo-code))
-      ((equal? (car neo-code) 'math) (neo-math-code-parser neo-code))
-      ((equal? (car neo-code) 'ask) (neo-ask-code-parser neo-code))
-      ((equal? (car neo-code) 'function) (neo-function-code-parser neo-code))
-      ((equal? (car neo-code) 'call) (neo-call-code-parser neo-code))
-      ((equal? (car neo-code) 'local-vars) (neo-let-code-parser neo-code))
-      (else (map neo-parser neo-code))
-      )
-    )
-  )
-
-
 (define neo-bool-code-parser
   (lambda (neo-code)
      (if (equal? (length neo-code) 3)
