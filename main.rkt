05f07f11080dee467725c7c5cb12a19cc6b5d150
Bdown4587 <55153467+Bdown4587@users.noreply.github.com>
2022-11-11 16:52:19 -0500

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -3,17 +3,13 @@
 (require "utility.rkt")
 (require "runner.rkt")
 (require "parcer.rkt")
+(require "variable_env.rkt")
 
-(define parsed-neo-code (neo-parser '(call (function (a) b) (5))))
+(define env '((global (a 1) (b 2) (c 5))))
 
+;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
+(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
+(displayln (neo-parser sample-code))
+(define parsed-neo-code (neo-parser sample-code))
 (run-neo-parsed-code parsed-neo-code env)
-
-;((r 5) (a 1) (b 2) (c 5))
-function fun1(a){
-  return d;
-}
-fun1(5);                          
-                         
-
-
  
