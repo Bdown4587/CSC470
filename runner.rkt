1d3477324f481cfc5937360895ffc01299534961
Bdown4587 <55153467+Bdown4587@users.noreply.github.com>
2022-11-11 16:57:04 -0500

    Update runner.rkt

--- runner.rkt
+++ runner.rkt
@@ -1,66 +1,176 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname runner) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 #lang racket
 (require "utility.rkt")
-(define resolve
-  (lambda (environment varname)
+(define resolve_scope;((a 1) (b 2) (c 5)), it gives two kinds of result. found return a value
+  (lambda (scope varname)
     (cond
-      ((null? environment) #false)
-      ((equal? (caar environment) varname) (cadar environment))
-      (else (resolve (cdr environment) varname))
-      )
-    )
-  )
-
-(define extend-env
-  (lambda (list-of-varname list-of-value env) 
-    (cond
-      ((null? list-of-varname) env)
-      ((null? list-of-value) env)
-      (else (extend-env (cdr list-of-varname) (cdr list-of-value)
-       (cons (list (car list-of-varname)
-                   (car list-of-value))
-             env)))
-             
-(define run-bool-parsed-code
-  (lambda(parsed-code env)
-    (let ((op (elementAt parsed-code 0))
-          (num1 (run-neo-parsed-code (elementAt parsed-code 1) env))
-          (num1 (run-neo-parsed-code (elementAt parsed-code 2) env)))
-            (cond
-              ((equal? op '>) (> num1 num2))
-              ((equal? op '<) (< num1 num2))
-              ((equal? op '>=) (>= num1 num2))
-              ((equal? op '<=) (<= num1 num2))
-              ((equal? op '==) (= num1 num2))
-              ((equal? op '!=) (not num1 num2))
-              ((equal? op '&&) (and num1 num2))
-              (else (not num1))       
-          )
-       )
-   )
-)            
-(define run-math-exp
-  (lambda (op num1 num2)
-    (cond
-      ((equal? op '+) (+ num1 num2))
-      ((equal? op '-) (- num1 num2))
-      ((equal? op '/) (/ num1 num2))
-      ((equal? op '//) (quotient num1 num2))
-      ((equal? op '%) (modulo num1 num2))
-      (else #false)       
+      ((null? scope) #false)
+      ((equal? (caar scope) varname) (cadar scope))
+      (else (resolve_scope (cdr scope) varname))
       )
     )
   )
   
-  (define run-let-exp
-    (lambda (parsed-code env)
-      (let ((list-of-name getVarnames (elementAt parsed-code 1))
-            (list-of-values getValues (elementAt parsed-code 1))
-            (new-env (extend-env list-of-names list-of-values env))
-            (body (elementAt parsed-code 2)))
-      (run-neo-parsed-code parsed-code env)
+(define resolve_env
+  (lambda (environment varname)
+    (cond
+      ((null? environment) #false)
+      ((null? (car environment)) (resolve_env (cdr environment) varname))
+      ((equal? 'global (car (car environment))) (resolve_scope (cdr (car environment)) varname))
+      (else (let ((resolved_result (resolve_scope (car environment) varname)))
+              (if (equal? resolved_result #false)
+                  (resolve_env (cdr environment) varname)
+                  resolved_result
+                  )
+              )
+       )
       )
     )
-(provide (all-defined-out)
+  )
+                                
+(define extend-scope
+  (lambda (list-of-varname list-of-value scope) 
+    (cond
+      ((null? list-of-varname) scope)
+      ((null? list-of-value) scope)
+      (else (extend-scope (cdr list-of-varname) (cdr list-of-value)
+       (cons (list (car list-of-varname)
+                   (car list-of-value))
+             scope)))
+      )
+    )
+  )
+
+(define push_scope_to_env
+  (lambda (list-of-varname list-of-value env)
+    (let ((new_scope (extend-scope list-of-varname list-of-value '()))
+          (pop_off_env (pop_env_to_global_scope env))) 
+      (cons new_scope pop_off_env) 
+      )
+    )
+  )
+
+(define pop_env_to_global_scope
+  (lambda (env)
+    (cond
+      ((null? env) #false)
+      ((equal? (length env) 1)
+       (if (equal? (car (car env)) 'global) env
+           #false))
+      (else (pop_env_to_global_scope (cdr env)))
+      )
+    )
+  )
+
+(define extend_local_scope
+  (lambda (list-of-varname list-of-value env)
+    (cond
+      ((null? env) #false)
+      ((equal? (caar env) 'global) (push_scope_to_env list-of-varname list-of-value env))
+      (else (cons (extend-scope list-of-varname list-of-value (car env))
+                  (pop_env_to_global_scope env)))
+     )
+    )
+  )
+
+(define run-neo-parsed-code
+  (lambda (parsed-code env)
+    (cond
+      ((null? parsed-code) '())
+      ((equal? (car parsed-code) 'num-exp)
+       (cadr parsed-code))
+      ((equal? (car parsed-code) 'var-exp)
+       (resolve_env env (cadr parsed-code)))
+      ((equal? (car parsed-code) 'bool-exp) (run-bool-parsed-code (cdr parsed-code) env))
+      ((equal? (car parsed-code) 'math-exp)
+       (run-math-exp (cadr parsed-code)
+                     (run-neo-parsed-code (caddr parsed-code) env)
+                     (run-neo-parsed-code (cadddr parsed-code) env)))
+      ((equal? (car parsed-code) 'ask-exp)
+       (if (run-neo-parsed-code (cadr parsed-code) env)
+           (run-neo-parsed-code (caddr parsed-code) env)
+           (run-neo-parsed-code (cadddr parsed-code) env)))
+      ((equal? (car parsed-code) 'func-exp)
+       (run-neo-parsed-code (cadr (caddr parsed-code)) env))
+      ((equal? (car parsed-code) 'let-exp)
+       (run-let-exp parsed-code env))
+      (else (run-neo-parsed-code
+             (cadr parsed-code) 
+             (push_scope_to_env (cadr (cadr (cadr parsed-code)))
+                                (map (lambda (exp) (run-neo-parsed-code exp env)) (caddr parsed-code))
+                                env
+                                )
+             )
+         )            
+      )
+    ) 
+  )
+
+(define run-bool-parsed-code
+  (lambda(parsed-code env)
+    (let ((op (elementAt parsed-code 0))
+           (num1 (run-neo-parsed-code (elementAt parsed-code 1) env))
+           (num2 (run-neo-parsed-code (elementAt parsed-code 2) env)))
+           (cond
+             ((equal? op '>) (> num1 num2))
+             ((equal? op '<) (< num1 num2))
+             ((equal? op '>=) (>= num1 num2))
+             ((equal? op '<=) (<= num1 num2))
+             ((equal? op '==) (= num1 num2))
+             ((equal? op '!=) (not (= num1 num2)))
+             ((equal? op '&&) (and num1 num2))
+             ((equal? op '||) (or num1 num2))
+             (else (not num1))
+        )
+      )
+    )
+  )
+
+(define run-math-exp
+  (lambda (op num1 num2)
+    (cond
+      ((equal? op '+) (+ num1 num2)) 
+      ((equal? op '-) (- num1 num2))  
+      ((equal? op '*) (* num1 num2)) 
+      ((equal? op '/) (/ num1 num2))  
+      ((equal? op '//) (quotient num1 num2)) 
+      ((equal? op '%) (modulo num1 num2)) 
+      (else #false)
+      )
+    )
+  )
+
+    
+
+(define run-let-exp
+  (lambda (parsed-code env)
+    (let* ((resolved-var-list (map (lambda (pair)
+                                     (list (car pair) (run-neo-parsed-code (cadr pair) env)))
+                                   (elementAt parsed-code 1)))
+           (list-of-names (getVarnames (elementAt parsed-code 1)))
+          (list-of-values (getValues resolved-var-list))
+          (new_env (extend_local_scope list-of-names list-of-values env))
+          (body (elementAt parsed-code 2)))
+    (run-neo-parsed-code body new_env)
+    )
+  )
+)
+(define cascade-update-env
+  (lambda (parsed-scope env)
+    (if (null? parsed-scope) env
+        (let* (
+               (original-local-scope (if (equal? (car (car env)) 'global) '() (car env)))
+               (varname (caar parsed-scope))
+               (var_value (run-neo-parsed-code (cadr (car parsed-scope)) env))
+               (pop_off_env (pop_env_to_global_scope env))
+               (new_env (list (cons (list varname var_value) original-local-scope)
+                            (pop_off_env)))
+             )
+          (cascade-update-env (cdr parsed-scope) new_env)
+          )
+      )
+    )
+  )
+
+(provide (all-defined-out))
