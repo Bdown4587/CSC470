4fd0bc3dbe92ef7aa929464fa9afa24db3ce55e8
Bdown4587 <55153467+Bdown4587@users.noreply.github.com>
2022-12-01 11:26:10 -0500

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -1,4 +1,4 @@
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname main) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
 #lang racket
 (require "utility.rkt")
 (require "runner.rkt")
@@ -7,9 +7,12 @@
 
 (define env '((global (a 1) (b 2) (c 5))))
 
-;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
-(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
-(displayln (neo-parser sample-code))
+(define sample-code '(block (assign i 0)
+                            (while (bool < i 10)
+                                   (block (assign a (math + i 1))
+                                          (assign i a)
+                                          (print i))
+                            )))
+;(displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
 (run-neo-parsed-code parsed-neo-code env)
- 
